0b31322b04dc313f6c55086941c49200fa229340
Urki Aristu <aristu.138167@e.unavarra.es>
2023-03-06 12:37:51 +0000

    Probando grumphp

--- src/Calculator.php
+++ src/Calculator.php
@@ -6,7 +6,7 @@ class Calculator
 {
     function add(int $number1, int $number2): int
     {
-        return $number1+$number2;
+        return $number1+$number
     }
 
     function multiply(int $number1, int $number2): int
